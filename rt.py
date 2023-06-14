3f970e20c8b81105cca7b374cfd869977949bb1d
BhanuBokka <bhanuteja9019bokka@gmail.com>
2023-06-14 17:11:00 +0530

    pass

--- rt.py
+++ rt.py
@@ -5,5 +5,5 @@ conn=sqlite3.connect("Patient1.db")
 cur=conn.cursor()
 
 #cur.execute("CREATE TABLE PATIENTSINFO(Name varchar(30),Result varchar(20) )")
-cur.execute("INSERT INTO PATIENTSINFO VALUES('SHALINI','DIABETIC')")
+cur.execute("INSERT INTO PATIENTSINFO VALUES('SHAL',' NO DIABETIC')")
 conn.commit()
