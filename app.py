60a0c29d551706c8ea169b7aad45a2b4b03f5a11
BhanuBokka <bhanuteja9019bokka@gmail.com>
2023-06-14 16:23:20 +0530

    1

--- app.py
+++ app.py
@@ -34,6 +34,18 @@ def predict():
     else:
         return render_template('predict.html')
 
+@app.route('/patients',methods=['GET','POST'])
+def showpatient():
+    conn=sqlite3.connect('patient1.db')
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM PATIENTSINFO")
+    data=[]
+    for i in cur.fetchall():
+        patient={}
+        patient['Name']=i[0]
+        patient['Result']=i[1]
+        data.append(patient)
+    return render_template('showpatient.html',data=data)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=False,port=5500)
---
+++ rt.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn=sqlite3.connect("Patient1.db")
+
+cur=conn.cursor()
+
+#cur.execute("CREATE TABLE PATIENTSINFO(Name varchar(30),Result varchar(20) )")
+cur.execute("INSERT INTO PATIENTSINFO VALUES('SHALINI','DIABETIC')")
+conn.commit()
